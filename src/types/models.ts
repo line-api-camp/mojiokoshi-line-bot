0ce8def3a09db508142e58496ad15547fe87f574
hyodoblog <yusei.hyodo@gmail.com>
2023-02-16 23:12:33 +0900

    ✨ 初期化時のデータを置いた

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,11 +1,17 @@
 import { Timestamp } from 'firebase-admin/firestore'
 
 interface Base {
-  id: string
-  createdAt: Timestamp
-  updatedAt: Timestamp
+  id?: string
+  createdAt?: Timestamp
+  updatedAt?: Timestamp
 }
 
 export interface User extends Base {
   point: number
+}
+
+export const initUserData = (): User => {
+  return {
+    point: 0
+  }
 }
