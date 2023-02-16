40c573eb8d540028217e03e8f3c5c73ab56addee
hyodoblog <yusei.hyodo@gmail.com>
2023-02-16 23:35:28 +0900

    🚑 stripe webhook func add.

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ import '~/utils/firebase/index'
 if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'lineBot') {
   exports.lineBot = require('./funcs/line-bot')
 }
+
+if (!process.env.FUNCTION_TARGET || process.env.FUNCTION_TARGET === 'stripeWebhook') {
+  exports.stripeWebhook = require('./funcs/webhook-stripe')
+}
