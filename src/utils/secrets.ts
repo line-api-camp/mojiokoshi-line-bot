407e2d31be7e59c1d1841f5d4a5c590ba71a3a33
hyodoblog <yusei.hyodo@gmail.com>
2023-02-17 18:20:19 +0900

    🦺 Cloud Vision APIの下準備

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -4,3 +4,9 @@ import 'dotenv/config'
 
 export const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN as string
 export const LINE_SECRET = process.env.LINE_SECRET as string
+
+// Google
+
+export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY as string
+export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL as string
+export const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID as string
