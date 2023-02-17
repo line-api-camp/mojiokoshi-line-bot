168eb70d4a336ac4b667277afcc027aae399f30c
hyodoblog <yusei.hyodo@gmail.com>
2023-02-17 18:14:37 +0900

    🦺 画像を扱う準備完了

--- src/funcs/line-bot/handlers/messages/image.ts
+++ src/funcs/line-bot/handlers/messages/image.ts
@@ -1,12 +1,15 @@
 import { ImageEventMessage, MessageEvent } from '@line/bot-sdk'
 
+import { getMessageContentWithBuffer } from '~/domains/line.domain'
 import { errorLogger } from '~/utils/util'
 
 export const messageImageHandler = async (event: MessageEvent): Promise<void> => {
   try {
-    const { id } = event.message as ImageEventMessage
+    const { id: messageId } = event.message as ImageEventMessage
 
-    console.info(id)
+    const imageBuffer = await getMessageContentWithBuffer(messageId)
+
+    console.info(imageBuffer)
   } catch (err) {
     errorLogger(err)
     throw new Error('message image handler')
